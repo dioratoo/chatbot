9a0952f128ea2c4af0da87e30080048e359fec1c
dioratoo <rafael.diorato@mhpsit.com>
2025-01-16 15:24:57 -0500

    ...

--- faq.js
+++ faq.js
@@ -1,6 +1,12 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./faq.db");
+const db = new sqlite3.Database("./faq.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err.message);
+  } else {
+    console.log("Conectado ao banco de dados SQLite.");
+  }
+});
 
 db.serialize(() => {
   // Criação da tabela FAQ
